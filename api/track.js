c8ce42def0ef288d1ebd2fe982f5052d3839e9ed
bricksfawk1244-source <bricksfawk1244@gmail.com>
2026-05-29 22:39:43 +0900

    Update track.js

--- api/track.js
+++ api/track.js
@@ -1,8 +1,12 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
 
-  const { event, page } = req.body;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  console.log("TRACK:", event, page);
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
   return res.status(200).json({ ok: true });
 }
